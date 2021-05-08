ea8ccde68b87a500fca329d244008fa2aac7251a
oreste-abizera <oresteabizera11@gmail.com>
2021-05-08 15:45:42 +0200

    url based on NODE_ENV

--- src/utils/url.ts
+++ src/utils/url.ts
@@ -1,3 +1,6 @@
-const url = "http://localhost:7000";
+const url =
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:7000"
+    : "https://ftvid.herokuapp.com";
 
 export default url;
