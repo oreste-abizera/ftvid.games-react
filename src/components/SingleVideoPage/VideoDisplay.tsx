6d0b036c14bc9adb7d2916103f324f593a1d6b45
oreste-abizera <oresteabizera11@gmail.com>
2021-05-09 13:44:28 +0200

    display first video

--- src/components/SingleVideoPage/VideoDisplay.tsx
+++ src/components/SingleVideoPage/VideoDisplay.tsx
@@ -1,10 +1,41 @@
 import React from "react";
+import styled from "styled-components";
+import { timeAgo } from "../../utils/helperFunctions";
 import { Match } from "../../utils/types";
 
 interface Props {
   match: Match;
 }
 
-export const VideoDisplay = (props: Props) => {
-  return <div>{props.match.embed}</div>;
+export const VideoDisplay = ({ match }: Props) => {
+  const { videos } = match;
+  if (videos.length === 0) {
+    return <div>no video found</div>;
+  }
+  const [firstVideo] = videos;
+  return (
+    <VideoDisplayWrapper>
+      <div>
+        <div
+          dangerouslySetInnerHTML={{ __html: firstVideo.embed }}
+          className="video__container"
+        ></div>
+        <h4>{`${match.title} (${firstVideo.title})`}</h4>
+        <p>{timeAgo(match.date)}</p>
+      </div>
+    </VideoDisplayWrapper>
+  );
 };
+
+const VideoDisplayWrapper = styled.div`
+  width: 60%;
+  margin: 1rem auto;
+
+  > div > h4 {
+    margin: 0.4rem 0;
+  }
+
+  @media screen and (max-width: 768px) {
+    width: 100%;
+  }
+`;
