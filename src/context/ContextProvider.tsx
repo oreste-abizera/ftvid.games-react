9f862da60497b13021136018fe618768a7274e70
oreste-abizera <oresteabizera11@gmail.com>
2021-05-09 12:43:12 +0200

    load new data on scroll to bottom

--- src/context/ContextProvider.tsx
+++ src/context/ContextProvider.tsx
@@ -9,19 +9,39 @@ const Context = React.createContext({});
 export function ContextProvider({ children }: Props): ReactElement {
   const [matches, setmatches] = React.useState<Array<Match>>([]);
   const [pageStatus, setpageStatus] = React.useState<PageStatus>();
+  const [loading, setloading] = React.useState(false);
 
   React.useEffect(() => {
     const fetchData = async () => {
+      setloading(true);
       let tempMatches = await loadVideos(1);
       let tempPageStatus = { ...tempMatches };
       delete tempPageStatus.docs;
       setmatches(tempMatches.docs);
       setpageStatus(tempPageStatus);
+      setloading(false);
     };
     fetchData();
   }, []);
+
+  async function loadNextPage() {
+    if (!loading) {
+      if (pageStatus?.hasNextPage) {
+        setloading(true);
+        let tempMatches = await loadVideos(pageStatus.nextPage);
+        let tempPageStatus = { ...tempMatches };
+        delete tempPageStatus.docs;
+        setmatches([...new Set([...matches, ...tempMatches.docs])]);
+        setpageStatus(tempPageStatus);
+        setloading(false);
+      }
+    }
+  }
+
   return (
-    <Context.Provider value={{ appName: "FtVid", matches, pageStatus }}>
+    <Context.Provider
+      value={{ appName: "FtVid", matches, pageStatus, loadNextPage }}
+    >
       {children}
     </Context.Provider>
   );
--- src/pages/VideosPage.tsx
+++ src/pages/VideosPage.tsx
@@ -1,15 +1,29 @@
-import { ReactElement } from "react";
+import { ReactElement, useContext } from "react";
 import styled from "styled-components";
 import Navbar from "../components/Navbar";
 import VideosList from "../components/VideosPage/VideosList";
+import Context from "../context/ContextProvider";
 
 export default function VideosPage(): ReactElement {
+  const contextData: any = useContext(Context);
+  const handleScroll = (e: any) => {
+    const bottom =
+      Math.floor(e.target.scrollHeight - e.target.scrollTop) <=
+      Math.floor(e.target.clientHeight);
+
+    if (bottom) {
+      contextData.loadNextPage();
+    }
+  };
   return (
-    <VideosPageWrapper>
+    <VideosPageWrapper onScroll={handleScroll}>
       <Navbar></Navbar>
       <VideosList></VideosList>
     </VideosPageWrapper>
   );
 }
 
-const VideosPageWrapper = styled.div``;
+const VideosPageWrapper = styled.div`
+  height: 100vh;
+  overflow-y: scroll;
+`;
