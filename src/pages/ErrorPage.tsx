772d50f0dd6b3945adc82b1f5d23ffe58ce638c0
oreste-abizera <oresteabizera11@gmail.com>
2021-05-10 12:03:11 +0200

    not found page

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -1,10 +1,33 @@
 import { ReactElement } from "react";
+import { Link } from "react-router-dom";
 import styled from "styled-components";
+import Navbar from "../components/Navbar";
 
 function ErrorPage(): ReactElement {
-  return <ErrorPageWrapper>404 == page not found</ErrorPageWrapper>;
+  return (
+    <ErrorPageWrapper>
+      <Navbar></Navbar>
+      <div className="error__container">
+        <p>Oops, You lost the way!</p>
+        <Link to="/">Return to Home</Link>
+      </div>
+    </ErrorPageWrapper>
+  );
 }
 
-const ErrorPageWrapper = styled.div``;
+const ErrorPageWrapper = styled.div`
+  .error__container {
+    width: 100%;
+    height: calc(100vh - 65px);
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .error__container p {
+    margin-bottom: 1rem;
+  }
+`;
 
 export default ErrorPage;
