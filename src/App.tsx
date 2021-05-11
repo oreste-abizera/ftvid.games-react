e528b57bfe0028abed3fc963b20f47219d53a1d0
oreste-abizera <oresteabizera11@gmail.com>
2021-05-11 22:22:13 +0200

    adding twitter icon link

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
+import { FaTwitter } from "react-icons/fa";
 import Routes from "./Routes";
 function App() {
   return (
     <div className="App">
       <Routes></Routes>
+      <a
+        href="https://twitter.com/FtVid_"
+        target="_new"
+        className="twitter__link"
+      >
+        <FaTwitter></FaTwitter>
+      </a>
     </div>
   );
 }
